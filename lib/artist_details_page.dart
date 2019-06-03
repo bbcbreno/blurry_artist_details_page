e2cf6dc94018345b565321805f4f8ca497ea7240
Breno Caldeira <breno.caldeira@gmail.com>
2019-06-03 17:31:24 -0300

    Add avatar Widget

--- lib/artist_details_page.dart
+++ lib/artist_details_page.dart
@@ -6,6 +6,44 @@ class ArtistDetailsPage extends StatelessWidget {
   ArtistDetailsPage(this.artist);
   final Artist artist;
 
+  Widget _buildContent() {
+    return SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+          _buildAvatar(),
+          _buildInfo(),
+          _buildVideoScroller(),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildAvatar() {
+    return Container(
+      margin: EdgeInsets.only(top: 32, left: 16),
+      padding: EdgeInsets.all(3.0),
+      width: 110,
+      height: 110,
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        border: Border.all(
+          color: Colors.white30,
+        ),
+      ),
+      child: ClipOval(
+        child: Image.asset(artist.avatar),
+      ),
+    );
+  }
+
+  Widget _buildInfo() {
+    return Container();
+  }
+
+  Widget _buildVideoScroller() {
+    return Container();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,6 +58,7 @@ class ArtistDetailsPage extends StatelessWidget {
             filter: ui.ImageFilter.blur(sigmaX: 5.0, sigmaY: 5.0),
             child: Container(
               color: Colors.black.withOpacity(0.5),
+              child: _buildContent(),
             ),
           )
         ],
